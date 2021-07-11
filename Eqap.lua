b6ac6048d999777f49808394f85c4e37ab9e2c0a
GG702GG <83525628+GG702GG@users.noreply.github.com>
2021-07-11 14:12:29 +0300

    Update Eqap.lua

--- Eqap.lua
+++ Eqap.lua
@@ -5431,7 +5431,7 @@ end
 
 if text == "ايديي" then
 GetUserID(msg.sender_user_id_,function(arg,data)
-if data.username_ then USERNAME = '@'..data.username_ else USERNAME = FlterName(data.first_name_..' '..(data.last_name_ or "")
+if data.username_ then USERNAME = '@'..data.username_ else USERNAME = FlterName(data.first_name_..' '..(data.last_name_ or "")) end
 end
 if text == 'تفعيل الايدي بالصوره' then
 
