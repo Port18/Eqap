fefd67ed90f3e905916ab1b6e6dd282499653a99
GG702GG <83525628+GG702GG@users.noreply.github.com>
2021-07-11 14:07:38 +0300

    Update Eqap.lua

--- Eqap.lua
+++ Eqap.lua
@@ -5431,12 +5431,8 @@ end
 
 if text == "ايديي" then
 GetUserID(msg.sender_user_id_,function(arg,data)
-if data.username_ then USERNAME = '@'..data.username_ else USERNAME = FlterName(data.first_name_..' '..(data.last_name_ or "")) end
-local USERCAR = utf8.len(USERNAME)
-SendMention(msg.chat_id_,data.id_,msg.id_,"USE : "..USERNAME.."\niD : "..data.id_.." ",37,USERCAR)  
-return false
-end)
- 
+if data.username_ then USERNAME = '@'..data.username_ else USERNAME = FlterName(data.first_name_..' '..(data.last_name_ or ""))
+end
 if text == 'تفعيل الايدي بالصوره' then
 
 if not Owner(msg) then
